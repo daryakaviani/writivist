f4f3c5e8860782e06e385441e8b50a0d45848915
Darya Kaviani <dkaviani@fb.com>
2020-07-15 12:48:46 -0700

    Added assets to create the TemplateCell view

---
+++ writivist/Views/TemplateCell.m
@@ -0,0 +1,13 @@
+//
+//  TemplateCell.m
+//  writivist
+//
+//  Created by dkaviani on 7/15/20.
+//  Copyright © 2020 dkaviani. All rights reserved.
+//
+
+#import "TemplateCell.h"
+
+@implementation TemplateCell
+
+@end

8ffb383ed3f40550a4fbdca85c4cda6ba48ee2ed
Darya Kaviani <dkaviani@fb.com>
2020-07-30 20:02:22 -0700

    Added login animation

--- writivist/View Controllers/LoginViewController.m
+++ writivist/View Controllers/LoginViewController.m
@@ -9,16 +9,24 @@
 #import "LoginViewController.h"
 #import "Parse/Parse.h"
 #import "User.h"
+#import "HyTransitions.h"
+#import "HyLoglnButton.h"
 
 @interface LoginViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *usernameField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordField;
+@property (strong, nonatomic) HyLoglnButton *loginButton;
 @end
 
 @implementation LoginViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.loginButton = [[HyLoglnButton alloc] initWithFrame:CGRectMake(self.view.center.x - self.passwordField.layer.frame.size.width/2, CGRectGetHeight(self.view.bounds) - self.view.frame.size.height/7, self.passwordField.layer.frame.size.width, 40)];
+    [self.loginButton setBackgroundColor:[[UIColor alloc]initWithRed:178/255.0 green:223/255.0 blue:219/255.0 alpha:1]];
+    [self.view addSubview:self.loginButton];
+    [self.loginButton setTitle:@"Login" forState:UIControlStateNormal];
+    [self.loginButton addTarget:self action:@selector(loginUser) forControlEvents:UIControlEventTouchUpInside];
     self.passwordField.secureTextEntry = YES;
     NSLog(@"%@", @"Name");
 }
@@ -28,27 +36,50 @@
     NSString *password = self.passwordField.text;
     [User logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
         if (error != nil) {
+            [self.loginButton ErrorRevertAnimationCompletion:^{}];
             NSLog(@"User log in failed: %@", error.localizedDescription);
             UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"User login failed:"
                    message:error.localizedDescription
             preferredStyle:(UIAlertControllerStyleAlert)];
             // create an OK action
-            UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) { }];
+            UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {}];
             // add the OK action to the alert controller
             [alert addAction:okAction];
             [self presentViewController:alert animated:YES completion:^{
             }];
         } else {
             NSLog(@"User logged in successfully");
-            [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+            [self.loginButton ExitAnimationCompletion:^{
+                [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+            }];
         }
     }];
 }
 
 
-- (IBAction)loginButton:(id)sender {
-    [self loginUser];
+#pragma mark Orientation
+-(BOOL)shouldAutorotate
+{
+    [super shouldAutorotate];
+    return NO;
 }
+- (NSUInteger) supportedInterfaceOrientations {
+    [super supportedInterfaceOrientations];
+    // Return a bitmask of supported orientations. If you need more,
+    // use bitwise or (see the commented return).
+    return UIInterfaceOrientationMaskPortrait;
+    // return UIInterfaceOrientationMaskPortrait | UIInterfaceOrientationMaskPortraitUpsideDown;
+}
+
+- (UIInterfaceOrientation) preferredInterfaceOrientationForPresentation {
+    [super preferredInterfaceOrientationForPresentation];
+    // Return the orientation you'd prefer - this is what it launches to. The
+    // user can still rotate. You don't have to implement this method, in which
+    // case it launches in the current orientation
+    return UIInterfaceOrientationPortrait;
+}
+
+
 - (IBAction)signupButton:(id)sender {
     [self performSegueWithIdentifier:@"signupsegue" sender:self];
 }
